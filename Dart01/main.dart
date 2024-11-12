6771f5b9558f826dbc848350b405f457ed9052f0
João Pedro Madeira <crepperdasrave@outlook.com>
2024-11-12 17:56:25 -0300

    DartVariables01

---
+++ Dart01/main.dart
@@ -0,0 +1,22 @@
+void main() {
+  //1 - Introdução a Variáveis
+
+  String variavelNome = "JoaoPedro";
+  print(variavelNome);
+
+  int valorVariavel = 10;
+  print(valorVariavel);
+
+  bool ehVerdadeiro = true;
+  print(ehVerdadeiro);
+
+  List<String> listaDePalavras = ["João", "Pedro", "Silva", "Madeira"];
+  print('${listaDePalavras[0]} ${listaDePalavras[1]} ${listaDePalavras[2]} ${listaDePalavras[3]}');
+
+  //2 - Introdução null safety
+  //3 - IF e SWITCH
+  //4 - Métodos e classes
+  //5 - Future, Async e Await
+  //6 - Orientação a objetos
+  //7 - Herança, Polimorfismo e Abstração
+}
