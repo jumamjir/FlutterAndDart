a32f672e5ea47feaaea5b01613d2e482d72634dd
João Pedro Madeira <crepperdasrave@outlook.com>
2024-11-16 12:18:30 -0300

    Commit Dart12Exceptions03

--- DartBasics03/Exceptions.dart
+++ DartBasics03/Exceptions.dart
@@ -1,11 +1,19 @@
 void main() {
- String user = "admin";
- String pass = "123";
+ Login login = new Login();
 
- if(pass.length <= 6) throw PasswordLengthError();
-   
+ login.logar();
 
+}
 
+class Login {
+
+  void logar() {
+  String user = "admin";
+  String pass = "123";
+
+  if(pass.length <= 6) throw PasswordLengthError();
+
+}
 }
 class PasswordLengthError implements Exception {
   PasswordLengthError();
